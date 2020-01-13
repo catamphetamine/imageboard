de413827ced24ab1b98b66d76fe5b6a6bd3c56b7
catamphetamine <purecatamphetamine@gmail.com>
2020-01-13 11:19:10 +0300

    Fixed `getCommentText()` messages

--- source/getCommentText.js
+++ source/getCommentText.js
@@ -1,17 +1,1 @@
-import getPostText from 'social-components/commonjs/utility/post/getPostText'
-
-/**
- * Generates a textual representation of comment's `content`.
- * This is just a `getPostText()` function re-exported from
- * `social-components` for convenience.
- * @param  {object} comment
- * @param  {object} [options] — See `options` description in `social-components/source/utility/post/getPostText`.
- * @return {string} [text]
- */
-export default function getCommentText(comment, options = {}) {
-	const { messages, ...rest } = options
-	return getPostText(comment, {
-		...rest,
-		messages: messages && messages.contentType
-	})
-}
+export { default as default } from 'social-components/commonjs/utility/post/getPostText'
