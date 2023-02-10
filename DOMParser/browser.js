fa4c3c77c7c7e1163b6dd2ad264cea0a0b7c2353
catamphetamine <kuchumovn@gmail.com>
2023-02-10 09:16:54 +0300

    .

--- DOMParser/browser.js
+++
@@ -1,5 +0,0 @@
-// `DOMParser` is supported in web browsers.
-export default function parseDocument(html) {
-	const document = new DOMParser().parseFromString(html, 'text/html')
-	return document.body
-}
--- DOMParser/node.js
+++
@@ -1,9 +0,0 @@
-import { DOMParser } from '@xmldom/xmldom'
-
-export default function parseDocument(html) {
-	// Web browsers seem to discard whitespace.
-	html = html.trim()
-	// Without the `<body/>` wrapper it would discard root-level text nodes.
-	const document = new DOMParser().parseFromString('<body>' + html + '</body>', 'text/html')
-	return document.childNodes[0]
-}
